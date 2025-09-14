36b9d4f4e1235d9155685323f697abae3da74a4b
Henrique Horbovyi <henrique.horbovyi@arctouch.com>
2025-09-14 20:45:25 -0300

    adding git plugin

---
+++ .config/nvim/lua/henriquehorbovyi/plugins/git.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", {})
+		end,
+	},
+}
